af873b3a7161d3a7cf6dadf483b83d305a45c036
Louism <8287251+louisfm16@users.noreply.github.com>
2023-09-04 17:00:19 -0700

    Fresh start, almost.

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// ! Must have the equal or greater amount of unique colors than there are books
-let bookColors = [
-  '#a0ced9', '#b7adcf', '#e9c46a', 
-  '#f4a261', '#e76f51', '#3A4F41', 
-  '#4F5D75', '#545863', '#134074', 
-  '#004643', '#DC965A', '#E9806E'];
 let myLibrary = [
   {
     title: 'Eloquent Javascript',
@@ -36,7 +30,7 @@ function docReady(fn) {
 } 
 
 docReady(function () {
-  renderBooksInLibrary();
+  // renderBooksInLibrary();
 });
 
 function Book(title, author, pageCount, isRead) {
@@ -56,47 +50,3 @@ function addBookToLibrary() {
 
   // Display Book function here
 }
-
-function generateRandBG(prevColor) {
-  let hexColor = bookColors[Math.floor(Math.random() * bookColors.length)];
-  
-  return hexColor === prevColor ? generateRandBG(prevColor) : hexColor;
-}
-
-function generateBook(title, author, prevBookBGColor) {
-  let bookEl = document.createElement('div');
-  let titleEl = document.createElement('h3');
-  let authorEl = document.createElement('h4');
-
-  bookEl.classList.add('book')
-
-  titleEl.classList.add('book--title');
-  titleEl.innerText = title;
-
-  authorEl.classList.add('book--author');
-  authorEl.innerText = author;
-
-  bookEl.appendChild(titleEl);
-  bookEl.appendChild(authorEl);
-  
-  bookEl.style.background = generateRandBG(prevBookBGColor);
-
-  return bookEl;
-}
-
-function renderBooksInLibrary() {
-  let bookshelfEl = document.getElementById('bookshelf-grid'); // #bookshelf-grid holds all book instances
-  bookshelfEl.innerHTML = '';
-
-  // * Note: Index added to implemement unique colors, still working on it!
-  myLibrary.forEach(book => {
-    // Grab the backround of the lattest book appended if one exists
-    let prevBookBGColor = bookshelfEl.lastChild ? bookshelfEl.lastChild.style.background : '#000';
-
-    bookshelfEl.appendChild(generateBook(book.title, book.author, prevBookBGColor));
-  });
-}
-
-
-// const myBook = new Book('Eloquent Javascript', 'Marijm Haverbeke', 450, false);
-// console.log(myBook.info());
